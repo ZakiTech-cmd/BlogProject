99bc6edd16d524a4b92a2cf73c1cb7a31ed55a7a
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-10-11 16:19:41 +0300

    Added articles app

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "articles",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
