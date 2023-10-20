ec5943d46a544ea6d183537ab287e3ff98def62d
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-10-20 11:44:50 +0300

    Login Logout API

--- blog_project/articles/serializers.py
+++ blog_project/articles/serializers.py
@@ -7,3 +7,8 @@ class ArticleSerializer(serializers.ModelSerializer):  # type: ignore
     class Meta:
         model = Article
         fields = "__all__"
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
--- blog_project/articles/views.py
+++ blog_project/articles/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate, login, logout
 from django.views import View
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import *
 
 # Create your views here.
 
@@ -13,3 +16,27 @@ class ArticlesView(View):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
+
+
+class LoginAPI(View):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data["username"]
+            password = serializer.validated_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return Response({"message": "Autentificare reușită."}, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {"message": "Autentificare eșuată. Verificați numele de utilizator și parola."},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAPI(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Deautentificare reușită."}, status=status.HTTP_200_OK)
